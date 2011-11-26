c1e46ca1acb4b6fb36d745880327eaaacc17c0ff
Albert Hoenigmann <hoenigmann@gmail.com>
2011-11-26 14:58:34 -0800

    added a solution upto 1.5

--- 1.1.work.scm
+++ 1.1.work.scm
@@ -30,3 +30,55 @@
 
 (define isi 15)
 isi
+
+;;; Conditional Expressions and Predicates
+;;; A Clause has a predicate and a consequent expression as the value of the clause
+;;; A Predicate evaluates to true or false. 
+(define (abs x)
+  (cond ((> x 0) x)
+	((= x 0) 0)
+	((< x 0) (- x))))
+
+(abs -3)
+
+(define (abs x)
+  (cond ((< x 0) (- x))
+	(else x)))
+(abs -4)
+
+(define (abs x) 
+  (if (< x 0)
+      (- x)
+      x))
+(abs -5)
+
+(and (> 7 5) (< 7 10))
+(define (>= x y)
+  (or (> x y) (= x y)))
+
+(>= 5 4)
+(>= 4 5)
+
+;;; Exercise 1.3
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7))
+)
+
+;;; Exercise 1.4
+(define (aplusabsb a b)
+ ((if (> b 0) + -) a b))
+(aplusabsb 5 -4)
+
+;;; Exercise 1.5
+;;; The behavior with applicative 
+;;; order observed will be that the call to p will be evaluated before the call to test
+;;; resulting in a infinite loop to evaluating p.
+;;; The behavior with normal order will produce a expansion with p not evaluated at the end point
+;;; the expansion.  When the evaluation is is carried out and the if predicate is found to be true
+;;; only the true consquent will be evaluated.
+
+
+
+
+
+
