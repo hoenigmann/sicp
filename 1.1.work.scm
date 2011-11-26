2af4c3438beb0343c8dd45f5602e3e261a6e3ff6
Albert Hoenigmann <hoenigmann@gmail.com>
2011-11-25 16:58:15 -0800

    another comment

--- 1.1.work.scm
+++ 1.1.work.scm
@@ -13,5 +13,17 @@
 (f 4)
 
 ;;; The substitution model for procedure application.
-;;;Evaluate the operator to get the procedure to be applied.
+;;; Evaluate the operator to get the procedure to be applied.
 ;;; Evaluate the operands to get the arguments to substitute for the formal parameters.
+;;; Model that determines "meaning" of the procedure application.
+;;; Typical interpreter won't substitute values for formal parameters.
+
+;;; Applicative vs. Normal Order
+;;; Fully Expand then follow by a series of reductions
+;;; is Normal Order Evaluation, possibly evaluating same expression
+;;; twice or more...
+;;; Evaluate Arguments then apply is Applicative Order Evaluation. Lisp uses this.
+;;; It can be shown that applicative order and normal order evaluation produce the same values.
+
+;;; Eager vs lazy, the order in which the substitution takes place, applicative or normal.
+;;; Ich hab etwas gefeunden...
