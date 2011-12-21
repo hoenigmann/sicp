9e6d5381c8a60b2b960368e04499a342fc3dae85
Albert Hoenigmann <hoenigmann@gmail.com>
2011-12-20 21:27:41 -0800

    find the fixed point of a mathematical function given a first guess

---
+++ fixed-point.scm
@@ -0,0 +1,11 @@
+
+(define (fixed-point f first-guess)
+  (define (close-enough a b)
+    (< (abs (- a b)) 0.00001))
+
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough guess next)
+	  guess
+	  (try next))))
+)
