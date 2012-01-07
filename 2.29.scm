d26e069f7484a5bdeea99c91b60aaf57461c9f73
Albert Hoenigmann <hoenigmann@gmail.com>
2012-01-07 08:54:55 -0800

    2.29.scm

--- 2.29.scm
+++ 2.29.scm
@@ -20,4 +20,44 @@
 
 (left-branch x)
 (right-branch x)
+(define (is-mobile? structure)
+  (pair? structure))
+(define (total-weight mobile)
+  (let ((left (branch-structure (left-branch mobile)))
+	(right (branch-structure (right-branch mobile))))
+    (+ 	(if (is-mobile? left)
+	    (total-weight left)
+	    left)
+	(if (is-mobile? right)
+	    (total-weight right)
+	    right))))
 
+(define (balanced? mobile)
+  (let ((left (branch-structure (left-branch mobile)))
+	(right (branch-structure (right-branch mobile)))
+	(left-length (branch-length (left-branch mobile)))
+	(right-length (branch-length (right-branch mobile))))
+    (and (=	(* left-length 
+		   (if (is-mobile? left)
+		       (total-weight left)
+		       left))
+		(* right-length
+		   (if (is-mobile? right)
+		       (total-weight right)
+		       right)))
+	 (balanced? left)
+	 (balanced? right))))
+
+
+(define branch (make-branch 5 4))
+(define mobile (make-mobile branch branch))
+(define branch-with-mobile (make-branch 6 mobile))
+
+(define mob1 (make-mobile branch branch-with-mobile))
+(define mob2 (make-mobile branch-with-mobile branch))
+(define branch1 (make-branch 3 mob1))
+(define branch2 (make-branch 9 mob2))
+
+(define root (make-mobile branch1 branch2))
+(total-weight root)
+(balanced root)
