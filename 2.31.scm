de035b9d8fc78bd4103d44145a8d9466f9c4e9e2
Albert Hoenigmann <hoenigmann@gmail.com>
2012-01-08 09:33:16 -0800

    2.30 2.31 scale-tree

---
+++ 2.31.scm
@@ -0,0 +1,14 @@
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+     (if (pair? sub-tree) 
+	 (tree-map proc sub-tree)
+	 (proc sub-tree))) 
+       tree))
+
+
+
+
+
+
+
+(define (square-tree tree) (tree-map square tree))
--- map.scm
+++ map.scm
@@ -2,3 +2,7 @@
   (if (null? items)
       '()
       (cons (proc (car items)) (map proc (cdr items)))))
+
+
+;;; map abstracts the idea of performing something to every item in a list. Or, to every sub structure in the list, if the function
+;;; passed to map accounts for and has knowledge of the sub structure.
